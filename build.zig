6acbe49e24eaf86fd1506031cf56b37d4e81bf2f
Zadock Maloba <55388270+zadockmaloba@users.noreply.github.com>
2024-12-18 17:19:16 +0300

    Collision detection and initial works on WASM

--- build.zig
+++ build.zig
@@ -1,7 +1,20 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
-    const target = b.standardTargetOptions(.{});
+    //const optn_step = b.addOptions();
+    //optn_step.addOption(bool, "build_wasm", false);
+    var build_wasm_opt = false;
+    if (b.option(bool, "build_wasm", "Build wasm module")) |val| {
+        build_wasm_opt = val;
+    }
+    const target = if (build_wasm_opt) b.standardTargetOptions(
+        .{
+            .default_target = .{
+                .cpu_arch = .wasm32,
+                .os_tag = .macos,
+            },
+        },
+    ) else b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
     const raylib_dep = b.dependency("raylib_zig", .{
@@ -13,6 +26,12 @@ pub fn build(b: *std.Build) void {
     const raygui = raylib_dep.module("raygui"); // raygui module
     const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
 
+    const lib = b.addStaticLibrary(.{
+        .name = "znake-lib",
+        .root_source_file = b.path("src/root.zig"),
+        .target = target,
+        .optimize = .ReleaseSmall,
+    });
     const exe = b.addExecutable(.{
         .name = "znake",
         .root_source_file = b.path("src/main.zig"),
@@ -23,6 +42,12 @@ pub fn build(b: *std.Build) void {
     exe.linkLibrary(raylib_artifact);
     exe.root_module.addImport("raylib", raylib);
     exe.root_module.addImport("raygui", raygui);
+
+    lib.linkLibrary(raylib_artifact);
+    lib.root_module.addImport("raylib", raylib);
+    lib.root_module.addImport("raygui", raygui);
+
+    b.installArtifact(lib);
 
     b.installArtifact(exe);
     const run_cmd = b.addRunArtifact(exe);
